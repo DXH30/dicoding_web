520c4af99c8eaa87d4dbe998949a36c664c59e89
DXH30 <dhs0223@gmail.com>
2020-09-21 10:41:50 +0800

    Tambah fungsi kalkulator sederhana di sini

--- fungsi.js
+++ fungsi.js
@@ -6,3 +6,20 @@ function fungsi() {
 function berita() {
     alert("Selamat datang "+nama);
 }
+
+// Fungsi untuk kalkulator sederhana
+function tambah(float a, float b) {
+    return a+b;
+}
+
+function kurang(float a, float b) {
+    return a-b;
+}
+
+function kali(float a, float b) {
+    return a*b;
+}
+
+function bagi(float a, float b) {
+    return a/b;
+}
