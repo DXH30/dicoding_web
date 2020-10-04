480c1a8d9f6b800dd42a7416c4c7c849528e9320
DXH30 <dhs0223@gmail.com>
2020-10-04 10:01:22 +0800

    Upload ini dah dulu

--- fungsi.js
+++ fungsi.js
@@ -1,25 +1,9 @@
-var nama = "Didik Hadumi Setiaji";
-function fungsi() {
-    console.log(nama);
+function tampilkankontak() {
+    var dropdowns = document.getElementById("dropdownnya");
+    dropdowns.style.display = "block";
 }
 
-function berita() {
-    alert("Selamat datang "+nama);
-}
-
-// Fungsi untuk kalkulator sederhana
-function tambah(float a, float b) {
-    return a+b;
-}
-
-function kurang(float a, float b) {
-    return a-b;
-}
-
-function kali(float a, float b) {
-    return a*b;
-}
-
-function bagi(float a, float b) {
-    return a/b;
+function sembunyikankontak() {
+    var dropdowns = document.getElementById("dropdownnya");
+    dropdowns.style.display = "none";
 }
